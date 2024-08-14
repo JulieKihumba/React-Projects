5db5d2529b6f30f24b08b5fef0ecaf89929b2f4f
JulieKihumba <151759770+JulieKihumba@users.noreply.github.com>
2024-08-14 11:35:37 +0300

    scrimba tailwind installation

---
+++ SCRIMBA/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaReact } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div>
+        <FaReact />
+        <h1>ReactFacts</h1>
+    </div>
+  )
+}
+
+export default Navbar
